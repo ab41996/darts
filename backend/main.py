92a3e5ba00b6a2dc90adfe9d607b09c1e59df562
ab41996 <abhakta@bink.com>
2024-02-20 15:59:25 +0000

    creating a back end and getting some end points

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,35 @@
-#%%
 import re
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 
-#%%
+app = FastAPI()
+
+# Mount the directory containing index.html as a static directory
+# app.mount("/", StaticFiles(directory="templates", html=True), name="static")
+
+matches = {1:{}}
+
 class Throw:
     def __init__(self, player_name, base_number, multiplier):
         self.player_name = player_name
         self.base_number = base_number
         self.multiplier = multiplier
-        self.score = base_number*multiplier
+        self.score = base_number * multiplier
 
         if self.score is not None:
             print(f"{self.player_name} scored {self.score}")
 
     @classmethod
-    def from_input(cls, player_name):
-        throw_input = input(f"Enter throw for {player_name}: e.g. t20")
+    def from_input(cls, player_name, throw_input):
         letters = ''.join(re.findall(r'[a-zA-Z]', throw_input))
         numbers = int(''.join(re.findall(r'[0-9]', throw_input)))
         base_number = numbers
-        multiplier = { "":1,"d":2,"t":3, "D":2, "T":3}[letters]
+        multiplier = {"": 1, "d": 2, "t": 3, "D": 2, "T": 3}[letters]
         return cls(player_name, base_number, multiplier)
 
-throw1 = Throw(player_name="hayes", base_number=20, multiplier=3)
-#%%
-throw1.score
-# %%
+
 class Visit:
     def __init__(self, player_name):
         self.player_name = player_name
@@ -36,15 +41,7 @@ class Visit:
             print("Max throws reached")
         else:
             self.throws.append(throw)
-#%%
 
-visit1 = Visit("hayes")
-visit1.add_throw(Throw("hayes", 20, 3))
-visit1.add_throw(Throw("hayes", 20, 3))
-visit1.add_throw(Throw("hayes", 20, 3))
-visit1.add_throw(Throw("hayes", 20, 3))
-
-# %%
 
 class Leg:
     def __init__(self, player1_name, player2_name):
@@ -71,40 +68,13 @@ class Leg:
         self.player1_score = sum(throw.score for visit in self.player1_visits for throw in visit.throws)
         self.player2_score = sum(throw.score for visit in self.player2_visits for throw in visit.throws)
         print(f"Scores on the doors.. {self.player1_name}: {self.player1_score}, {self.player2_name}: {self.player2_score}")
-            
-
-#%%
-leg1 = Leg("hayes", "anand")
-
-visit2 = Visit("hayes")
-visit2.add_throw(Throw("hayes", 20, 3))
-visit2.add_throw(Throw("hayes", 20, 3))
-visit2.add_throw(Throw("hayes", 20, 3))
-visit2.add_throw(Throw("hayes", 20, 3))
-
-leg1.add_visit(visit2)
-
-visit3 = Visit("anand")
-visit3.add_throw(Throw("hayes", 10, 1))
-visit3.add_throw(Throw("hayes", 10, 1))
-visit3.add_throw(Throw("hayes", 10, 1))
-visit3.add_throw(Throw("hayes", 10, 1))
-
-leg1.add_visit(visit3)
-
-# visit4 = Visit("max")
-# visit4.add_throw(10, 1)
-# visit4.add_throw(5, 1)
-# visit4.add_throw(12, 1)
-
-# leg1.add_visit(visit4)
 
 
-# %%
-class Match:
+class Match():
     def __init__(self, player1_name, player2_name, type):
         self.player1_name = player1_name
         self.player2_name = player2_name
+        self.type = type
         self.legs = []
         self.winner = ""
 
@@ -117,24 +87,59 @@ class Match:
         else:
             raise ValueError(f"Invalid player: {leg.player1_name}")
 
-#%% Interactive section below!!
+@app.post("/throw/{player_name}")
+def add_throw(player_name: str, throw_input: str):
+    throw = Throw.from_input(player_name, throw_input)
+    return {"message": f"{player_name} scored {throw.score}"}
 
-player1_name = input("Enter name for Player 1: ")
-player2_name = input("Enter name for Player 2: ")
 
-leg1 = Leg(player1_name, player2_name)
+@app.post("/visit/{player_name}")
+def add_visit(player_name: str):
+    visit = Visit(player_name)
+    return {"message": f"{player_name} is up!!"}
 
-for _ in range(3):  # Assume each visit consists of 3 throws
-    visit = Visit(player1_name)
-    for _ in range(3):
-        throw = Throw.from_input(player1_name)
-        visit.add_throw(throw)
-    leg1.add_visit(visit)
 
-    visit = Visit(player2_name)
-    for _ in range(3):
-        throw = Throw.from_input(player2_name)
-        visit.add_throw(throw)
-    leg1.add_visit(visit)
+@app.post("/leg/{player1_name}/{player2_name}")
+def add_leg(player1_name: str, player2_name: str):
+    leg = Leg(player1_name, player2_name)
+    return {"message": f"{player1_name} vs {player2_name}!! Let's Gooo!!"}
+
+@app.get('/test/{id}')
+def test(id):
+    return {"test_id": id}
+
+@app.post("/match/{player1_name}/{player2_name}/{type}") #Operational
+def start_match(player1_name: str, player2_name: str, type: int):
+    match = Match(player1_name, player2_name, type)
+    match_id = player1_name+player2_name+str(type)
+    if match_id in matches:
+        return {"Match already exists"}
+    else:
+        matches[match_id] = match
+    return {"message": "{type} Match started {player1_name} vs {player2_name}!",
+            "match": matches[match_id]}
+
+@app.get("/get_match/{match_id}") #Operational
+def get_match(match_id: str):
+    return matches[match_id]
         
-#%%
+
+# Interactive section below!!
+
+# player1_name = input("Enter name for Player 1: ")
+# player2_name = input("Enter name for Player 2: ")
+
+# leg1 = Leg(player1_name, player2_name)
+
+# for _ in range(3):  # Assume each visit consists of 3 throws
+#     visit = Visit(player1_name)
+#     for _ in range(3):
+#         throw = Throw.from_input(player1_name)
+#         visit.add_throw(throw)
+#     leg1.add_visit(visit)
+
+#     visit = Visit(player2_name)
+#     for _ in range(3):
+#         throw = Throw.from_input(player2_name)
+#         visit.add_throw(throw)
+#     leg1.add_visit(visit)
